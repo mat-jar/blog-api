7f05500c19f387d3d30901df28ac5fa0921d7eaf
mat-jar <mtu07@poczta.fm>
2022-06-13 00:15:24 +0200

    Add permitted parameters to update user details

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,21 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
   before_action :configure_permitted_parameters
 
-  protected
-  def configure_permitted_parameters
-    #devise_parameter_sanitizer.permit(:account_update, keys: [:name, :role, :teacher_id])
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :role, :teacher_id) }
-  end
-
-
   private
 
   def sign_up(resource_name, resource)
   end
 
   def respond_with(resource, _opts = {})
-    #render json: { message: resource.to_s }
-    #render json: { message: resource.persisted?.to_s }
+    
     resource.persisted? ? register_success : register_failed
   end
 
@@ -28,7 +20,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render json: { message: "Signed up failure." },  status: :unprocessable_entity
 
   end
-
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :role, :teacher_id) }
+  end
 
 end
