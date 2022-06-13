8dc0d65d14f9f9d51647a1593c1c93ee3d69f0a1
mat-jar <mtu07@poczta.fm>
2022-06-13 14:05:34 +0200

    Add unique index to answer_times table with custom -> shorter name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_115333) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_085104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_115333) do
     t.integer "time_millisecond"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["flashcard_id", "round", "learning_session_id"], name: "answer_times_unique_index", unique: true
     t.index ["flashcard_id"], name: "index_answer_times_on_flashcard_id"
     t.index ["learning_session_id"], name: "index_answer_times_on_learning_session_id"
   end
@@ -103,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_115333) do
     t.bigint "teacher_id"
     t.datetime "last_request_at"
     t.boolean "logged_in", default: false
+    t.datetime "last_login_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["teacher_id"], name: "index_users_on_teacher_id"
