a109140f74a63013a44d05b1e99113d6e217ea77
mat-jar <mtu07@poczta.fm>
2022-06-13 02:39:02 +0200

    Fix get_word_average_answer_time method in AnalyseAnswerTime
    Incorectly used collection method on ActiveRecord::Relation object

--- app/lib/AnalyseAnswerTime.rb
+++ app/lib/AnalyseAnswerTime.rb
@@ -10,7 +10,7 @@ class AnalyseAnswerTime
   end
 
   def self.get_word_average_answer_time(word)
-    average_answer_time = Flashcard.where("front_text = ? or back_text = ?", word, word).joins(:answer_times).average(:time_millisecond).to_i
+    average_answer_time = AnswerTime.joins(:flashcard).where("front_text = ? or back_text = ?", word, word).average(:time_millisecond).to_i
   end
 
   def self.get_learning_session_average_answer_time(learning_session_id)
