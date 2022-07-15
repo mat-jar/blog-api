a21f80df3c63a88ae0a55b104ad6ed9f4e726275
mat-jar <mtu07@poczta.fm>
2022-07-15 13:36:55 +0200

    Removes unused http status for  CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::API
   rescue_from CanCan::AccessDenied do |exception|
    if current_user
      render status: :forbidden # 403
-   else
-     render status: :unauthorized # 401 unauthenticated
+   #else
+   # render status: :unauthorized # 401 unauthenticated
    end
  end
 
