523149e74d710781ba50f83f90a215c1372c9808
mat-jar <mtu07@poczta.fm>
2022-08-15 21:54:43 +0200

    Add ActiveStorage

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -3,4 +3,29 @@ class Flashcard < ApplicationRecord
   has_many :flashcard_set_settings_panels
   has_many :answer_times
   validates :front_text, :back_text, presence: true
+  has_one_attached :front_photo
+  has_one_attached :back_photo
+  validate :acceptable_flashcard_photo
+
+  def acceptable_flashcard_photo
+    return unless front_photo.attached?
+    return unless back_photo.attached?
+
+    unless front_photo.byte_size <= 300.kilobyte
+      errors.add(:front_photo, "is too big")
+    end
+    unless back_photo.byte_size <= 300.kilobyte
+      errors.add(:back_photo, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+
+    unless acceptable_types.include?(front_photo.content_type)
+      errors.add(:front_photo, "must be a JPEG or PNG")
+    end
+    unless acceptable_types.include?(back_photo.content_type)
+      errors.add(:back_photo, "must be a JPEG or PNG")
+    end
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
   has_many :students, class_name: "User", foreign_key: "teacher_id"
   belongs_to :teacher, class_name: "User", optional: true
   validate :is_teacher_and_student
+  has_one_attached :profile_photo
+  validate :acceptable_profile_photo
 
   def is_teacher_and_student
 
@@ -28,6 +30,19 @@ class User < ApplicationRecord
 
   def on_jwt_dispatch(token, payload)
     User.find(payload["sub"]).update(last_login_at: payload["last_login_at"])
+  end
+
+  def acceptable_profile_photo
+    return unless profile_photo.attached?
+
+    unless profile_photo.byte_size <= 300.kilobyte
+      errors.add(:profile_photo, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(profile_photo.content_type)
+      errors.add(:profile_photo, "must be a JPEG or PNG")
+    end
   end
 
 end
